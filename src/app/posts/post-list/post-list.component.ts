cc32a0dcfe108b46f9283c0b80dfb9801e05d0b5
L0rellei <florentinai@me.com>
2020-08-18 09:20:08 -0700

    set up post service for data sharing between components

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,12 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   templateUrl:'./post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy{
   // posts = [
   //   {title: 'First Post',
   //   content: 'This is the first post\'s content'},
@@ -16,6 +18,25 @@ export class PostListComponent {
   //   content: 'This is the third post\'s content'},
   // ];
   // posts = []
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  // postsService: PostsService;
+
+  constructor(public postsService: PostsService) {//passing public postsService automatically creates a property with that name
+
+  }
+  ngOnDestroy(): void {
+    // throw new Error("Method not implemented.");
+    this.postsSub.unsubscribe();
+  }
+  ngOnInit(): void {
+    // throw new Error("Method not implemented.");
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdatedListener()
+    .subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
 
 }
